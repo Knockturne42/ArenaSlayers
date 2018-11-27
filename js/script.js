8289afbdd028c1f84a727c30af8e79ff15697437
Knockturne42 <dcl.thibauds@18pixel.fr>
2018-11-27 15:55:29 +0100

    sauvegarde Jour 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var mainCharacter = document.getElementById('joueur');
+var myMaps = document.getElementsByTagName('main');
 mainCharacter.style.backgroundPositionX = '0px';
 mainCharacter.style.backgroundPositionY = '-64px';
+myMaps[0].style.backgroundPositionX = '50%';
+myMaps[0].style.backgroundPositionY = '50%';
 
 document.addEventListener("keydown", function(e){
 	console.log(e.keyCode);
@@ -8,22 +11,26 @@ document.addEventListener("keydown", function(e){
 	switch(key){
 		case 38:
 			console.log('haut');
+			moveMap(2)
 			mainCharacter.style.backgroundPositionX = '0px';
 			mainCharacter.style.backgroundPositionY = '0px';
 			movePerso();
 			break;
 		case 37:
 			console.log('gauche');
+			moveMap(1)
 			mainCharacter.style.backgroundPositionY = '-96px';
 			movePerso();
 			break;
 		case 39:
 			console.log('droite');
+			moveMap(0)
 			mainCharacter.style.backgroundPositionY = '-32px';
 			movePerso();
 			break;
 		case 40:
 			console.log('bas');
+			moveMap(3)
 			mainCharacter.style.backgroundPositionY = '-64px';
 			movePerso();
 			break;
@@ -39,6 +46,25 @@ document.addEventListener("keydown", function(e){
 			break;
 	}
 });
+
+function moveMap(direction) {
+	var posXActuel = parseInt(myMaps[0].style.backgroundPositionX);
+	var posYActuel = parseInt(myMaps[0].style.backgroundPositionY);
+	switch (direction) {
+		case 0:
+			myMaps[0].style.backgroundPositionX = (posXActuel+9)+'%';
+			break;
+		case 1:
+			myMaps[0].style.backgroundPositionX = (posXActuel-9)+'%';
+			break;
+		case 2:
+			myMaps[0].style.backgroundPositionY = (posYActuel-18)+'%';
+			break;
+		case 3:
+			myMaps[0].style.backgroundPositionY = (posYActuel+18)+'%';
+			break;
+	}
+}
 
 function movePerso() {
 	var posActuel = parseInt(mainCharacter.style.backgroundPositionX);
